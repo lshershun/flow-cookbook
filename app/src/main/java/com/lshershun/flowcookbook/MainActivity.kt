054cf93c55cddc875902470d3c4e4c473083412f
lshershun <lsherhsun@gmail.com>
2026-02-09 21:57:27 +0200

    initial commit

---
+++ app/src/main/java/com/lshershun/flowcookbook/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.lshershun.flowcookbook
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
